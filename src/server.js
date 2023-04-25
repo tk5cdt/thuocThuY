6f08bb2c6368911c72ba59e9b8ae5932e4d66aa1
Duc Truong <tk5cdt@gmail.com>
2023-04-25 15:03:29 +0700

    add env

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 import express from 'express'
 import configViewEngine from './configs/viewEngine'
+require('dotenv').config()
+const port = process.env.PORT || 3000
 
 const app = express()
-const port = 3000
+
 
 configViewEngine(app)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './FE/4T.html'));
+  });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
