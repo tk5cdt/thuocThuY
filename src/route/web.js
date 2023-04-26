4ecdd9ed5a17f2be4152197a29a13ca797b6ff07
Duc Truong <tk5cdt@gmail.com>
2023-04-27 01:04:11 +0700

    add homeController

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,10 @@
 import express from "express";
+import homeController, { getHompage } from "../controller/homeController";
 
 let router = express.Router();
 
 const initWebRoute = (app) => {
-    router.get('/', (req, res) => {
-        return res.render('index.ejs');
-    })
+    router.get('/', homeController.getHompage)
     
     router.get('/about', (req, res) => {
         return res.send('This is about page');
